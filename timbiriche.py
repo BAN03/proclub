0b416809d9e0efb1dddf783a9077e007c6ac4f15
BAN03 <angeldejesusatnasiodiaz@gmail.com>
2023-08-29 11:16:01 -0600

    updating the logic

--- timbiriche.py
+++ timbiriche.py
@@ -14,3 +14,13 @@ input = valores de M y N,
 
 salida = puntuajes de la partida
 """
+
+# tablero de la partida
+N = [0 for x in range(int(input())**2)]
+
+# Movimientos de la partida
+M = int(input())
+
+player1 = i%2 # 0
+player2 = i%2 # 1
+player1.score = player1.score + 1 # player 1 gets an extra turn, so 'i' needs to get extracted by 1
