3cd403a19c151f62a4ff4dfab817be4987454241
BAN03 <angeldejesusatnasiodiaz@gmail.com>
2023-08-29 10:40:24 -0600

    update fist code

---
+++ timbiriche.py
@@ -0,0 +1,16 @@
+"""
+longitud de array = N**2
+ejemplo: 
+N = 2
+array = [
+    [0,0]
+    [0,0]
+]
+
+M = cantidad de movimientos maximos en la partida
+
+input = valores de M y N,
+        dos enteros para el trazo de linea del jugador
+
+salida = puntuajes de la partida
+"""
